7243e062b0c6cf884da9f3525109b62bc8f58eac
Arthur Santos Costa <arthur_scosta@yahoo.com.br>
2016-08-03 08:29:27 -0300

    Change module.config.php to apoint to www.agenciadigitala.com.br

--- module/DABlog/config/module.config.php
+++ module/DABlog/config/module.config.php
@@ -2,29 +2,30 @@
 
 namespace DABlog;
 
-use Zend\ServiceManager\Factory\InvokableFactory;
-use Zend\Mvc\Router\Http\{Literal, Segment, Hostname};
+use Zend\Mvc\Router\Http\Hostname;
+use Zend\Mvc\Router\Http\Literal;
+use Zend\Mvc\Router\Http\Segment;use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
     'router' => [
         'routes' => [
             'dablog-home' => array(
-                'type'    => Hostname::class,
+                'type' => Hostname::class,
                 'options' => array(
-                    'route'    => 'digitala.local',
+                    'route' => 'www.agenciadigitala.com.br',
                     'defaults' => array(
                         'controller' => Controller\IndexController::class,
-                        'action'     => 'index',
+                        'action' => 'index',
                     ),
                 ),
             ),
             'dablog-landing-page' => array(
                 'type' => Literal::class,
                 'options' => array(
-                    'route'    => '/teste-landing',
+                    'route' => '/teste-landing',
                     'defaults' => array(
                         'controller' => Controller\IndexController::class,
-                        'action'     => 'landingPage',
+                        'action' => 'landingPage',
                     ),
                 ),
             ),
@@ -37,19 +38,19 @@ return [
     ],
     'view_manager' => [
         'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
         'template_map' => [
-            'layout/blog01'                 => __DIR__ . '/../view/layout/layout.blog.phtml',
-            'layout/landingTest'                 => __DIR__ . '/../view/layout/layout.landing.phtml',
+            'layout/blog01' => __DIR__ . '/../view/layout/layout.blog.phtml',
+            'layout/landingTest' => __DIR__ . '/../view/layout/layout.landing.phtml',
 
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
-        ]
+        ],
     ],
 ];
